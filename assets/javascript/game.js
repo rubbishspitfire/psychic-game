a7819e0e53c582c195cb38cc58026613a60440b5
Patience Condell <condellp@gmail.com>
2020-04-28 14:19:23 -0400

    Week 3 Homework (v.2)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,83 @@
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+// This array will hold what we guess
+var guessedLetters = [];
 
+// This variable will be randomly assigned one of the three letters
+var letterToGuess = null;
 
+// This is what we'll use to count down
+var guessesLeft = 9;
+
+// This is the counter for wins/losses
 var wins = 0;
 var losses = 0;
-var left =10;
-var guessesSoFar = [];
-var psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-
-var soFar = function() {
-    document.getElementById("guesses").innerHTML = "What you've guessed" + guessesSoFar.join(",");  
+// Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
+var updateGuessesLeft = function() {
+  // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
+  // (i.e. guessesLeft will get displayed in HTML)
+  document.querySelector("#guesses-left").innerHTML = guessesLeft;
 };
 
-var guessesLeft = function() {
-    document.getElementById("left").innerHTML = "Guesses you have left" + left;
+var updateLetterToGuess = function() {
+  // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
 };
 
-var newGame = function () {
-    guessesSoFar = [];
-    wins = 0;
-    losses = 0;
-    left = 10;
-    guessesLeft();
-    soFar();
-    document.getElementById("wins").innerHTML = "Wins" + wins;
-    document.getElementById("losses").innerHTML ="Losses" + losses;
-}
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas.
+  document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
 
-document.onkeyup = function(event) {
-    var userGuess = event.key.toLowerCase();
-    left--;
-    if (guessesSoFar.includes(userGuess)) {
-        alert("Hey you already guessed" + userGuess);
-    }
-  // I want to change the alert to where the user guesses the same letter twice nothing happens. Right now after the alert 
-  // the letter gets added to the array twice
-    guessesSoFar.push(userGuess);
-    soFar();
-    guessesLeft();
-    if (left > 0) {
+// Function will be called when we reset everything
+var reset = function() {
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+};
+
+// Execute on page load.
+updateLetterToGuess();
+updateGuessesLeft();
+
+// This function will capture the keyboard clicks.
+document.onkeydown = function(event) {
+  // It's going to reduce the guesses by one
+  guessesLeft--;
+
+  // Lowercase the letter
+  var letter = event.key.toLowerCase();
+
+  // Then add the guess to the guessedLetters array
+  guessedLetters.push(letter);
+
+  // Then its going to run the update functions
+  updateGuessesLeft();
+  updateGuessesSoFar();
 
 
-        if(userGuess === psychicLetter) {
-            wins++;
-            document.getElementById("wins").innerHTML = "Wins" + wins;
-        } else {
-            losses++;
-            document.getElementById("losses").innerHTML = "Losses" + losses;
-        }
+  // We'll check if there's a match.
+  if (letter === letterToGuess) {
 
-        
+    // If there is then we win and we'll update the HTML to display the win.
+    wins++;
+    document.querySelector("#wins").innerHTML = wins;
 
-    } else {
-        console.log("Game Over!");
-        newGame();
+    // Then we'll reset the game
+    reset();
+  }
 
-       }
-    };
+  // If we are out of guesses...
+  if (guessesLeft === 0) {
+
+    // Then we will loss and we'll update the HTML to display the loss.
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
+
+    // Then we'll call the reset.
+    reset();
+  }
+};
